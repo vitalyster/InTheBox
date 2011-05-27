16f9d8e190f4e47b56a3a9b844e5833f494e33e5
bouliii <bouliii@flexycore.com>
2011-05-27 12:24:15 +0000

    cleaning

--- InTheBoxSim/tools.h
+++ InTheBoxSim/tools.h
@@ -6,11 +6,11 @@
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-#import "toolsCinterface.h"
-
-@interface tools {
-    
-}
-
-@end
+//#import <Foundation/Foundation.h>
+//#import "toolsCinterface.h"
+//
+//@interface tools {
+//    
+//}
+//
+//@end
--- InTheBoxSim/tools.m
+++ InTheBoxSim/tools.m
@@ -6,7 +6,7 @@
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
-#import "tools.h"
+#import "toolsCinterface.h"
 
 
 char * getAppDocumentsPath ()
